be6ce7bb055b205a135560480345b5688d00fa93
wzwc <a30415918@qq.com>
2023-07-09 23:26:03 +0800

    修改3

--- src/main/java/com/flex/config/CorsConfig.java
+++ src/main/java/com/flex/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 
 @Configuration
-public class CorsConfig {a
+public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
