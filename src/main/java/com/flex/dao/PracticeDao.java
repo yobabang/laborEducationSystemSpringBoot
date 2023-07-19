23ea52b2a3691b1059da3f8b5305f6493d5e4d10
wzwc <a30415918@qq.com>
2023-07-19 16:05:09 +0800

    修改

--- src/main/java/com/flex/dao/PracticeDao.java
+++ src/main/java/com/flex/dao/PracticeDao.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Mapper
 public interface PracticeDao extends BaseMapper<Practice> {
+
     @Select("select * from practice,user,classes where user.user_id = practice.user_id and user.class_id = classes.class_id and classes.class_name = #{className}")
     public List<Practice> getByClass(String className);
 }
--- src/main/java/com/flex/service/PracticeService.java
+++ src/main/java/com/flex/service/PracticeService.java
@@ -1,11 +1,11 @@
 package com.flex.service;
 
 import com.flex.domain.Practice;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
+@Transactional
 public interface PracticeService {
 
     /**
