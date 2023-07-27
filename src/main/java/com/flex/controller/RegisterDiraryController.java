e8c61bf6f3b9d3c1f95807c548bfe1b7ea332068
wzwc <a30415918@qq.com>
2023-07-27 16:05:55 +0800

    123

---
+++ src/main/java/com/flex/controller/RegisterDiraryController.java
@@ -0,0 +1,11 @@
+package com.flex.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/regs")
+@Api(tags = "社会实践活动登记管理")
+public class RegisterDiraryController {
+}
--- src/main/java/com/flex/dao/RegisterDiraryDao.java
+++ src/main/java/com/flex/dao/RegisterDiraryDao.java
@@ -2,6 +2,8 @@ package com.flex.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.flex.domain.RegisterDirary;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RegisterDiraryDao extends BaseMapper<RegisterDirary> {
 }
